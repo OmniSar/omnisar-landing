3450ea4f1a185c35e74f79aeadfcafc668b603fc
Klimek Jakub <kqklimek@gmail.com>
2026-04-13 09:43:34 +0200

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `omnisar-landing`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-sass"]
+};
